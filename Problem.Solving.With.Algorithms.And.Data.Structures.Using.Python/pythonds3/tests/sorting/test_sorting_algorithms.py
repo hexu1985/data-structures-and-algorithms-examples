9662c06462a1b3a8afdc783fa7781eb828149089
hexu <hexu_1985@sina.com>
2026-08-17 14:58:24 +0800

    update Problem.Solving.With.Algorithms.And.Data.Structures.Using.Python/pythonds3/tests/sorting test_sorting_algorithms.py

--- Problem.Solving.With.Algorithms.And.Data.Structures.Using.Python/pythonds3/tests/sorting/test_sorting_algorithms.py
+++ Problem.Solving.With.Algorithms.And.Data.Structures.Using.Python/pythonds3/tests/sorting/test_sorting_algorithms.py
@@ -10,7 +10,7 @@ Karina E. Hoff, 2018
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 import pytest
 from random import randint
